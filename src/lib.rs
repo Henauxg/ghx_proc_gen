a7f4250fadaa7de4865be58f491b3751d56dd33d
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-11-22 21:39:41 +0100

    update ascii test case

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ pub enum ProcGenError {
 
 #[cfg(test)]
 mod tests {
+    use std::rc::Rc;
+
     use crate::{
         generator::{
             builder::GeneratorBuilder,
@@ -50,34 +52,41 @@ mod tests {
     fn generate_test_ascii() {
         tracing_subscriber::fmt::init();
         let models = vec![
-            NodeModel::new_2d(vec![2], vec![2], vec![2], vec![2]), // Mountain
-            NodeModel::new_2d(vec![1, 2], vec![1, 2], vec![1, 2], vec![1, 2]), // Forest
+            NodeModel::new_2d(vec![3], vec![3], vec![3], vec![3]), // Mountain
+            NodeModel::new_2d(vec![2, 3], vec![2, 3], vec![2, 3], vec![2, 3]), // Forest1
+            NodeModel::new_2d(vec![2, 3], vec![2, 3], vec![2, 3], vec![2, 3]), // Forest2
+            NodeModel::new_2d(vec![2, 1], vec![2, 1], vec![2, 1], vec![2, 1]), // Meadows
             NodeModel::new_2d(vec![0, 1], vec![0, 1], vec![0, 1], vec![0, 1]), // Beach
             NodeModel::new_2d(vec![0], vec![0], vec![0], vec![0]), // Sea
         ];
-        let rules = Rules::new_cartesian_2d(models);
-        let size_x = 2;
-        let size_y = 2;
-        let grid = Grid::new_cartesian_2d(size_x, size_y, false);
-        let mut generator = GeneratorBuilder::new()
-            .with_rules(rules)
-            .with_grid(grid)
-            .with_max_retry_count(10)
-            .with_rng(RngMode::Seeded(0))
-            .build();
-        let output = generator.generate().unwrap();
+        let rules = Rc::new(Rules::new_cartesian_2d(models));
+        let repeat_count = 1;
+        for _ in 0..repeat_count {
+            let size_x = 22;
+            let size_y = 10;
+            let grid = Grid::new_cartesian_2d(size_x, size_y, false);
+            let mut generator = GeneratorBuilder::new()
+                .with_shared_rules(Rc::clone(&rules))
+                .with_grid(grid)
+                .with_max_retry_count(250)
+                .with_rng(RngMode::Random)
+                .build();
+            let output = generator.generate().unwrap();
 
-        for y in (0..size_y).rev() {
-            for x in 0..size_x {
-                match output.get_2d(x, y).index {
-                    0 => print!("🗻"),
-                    1 => print!("🌲"),
-                    2 => print!("🟨"),
-                    3 => print!("🌊"),
-                    others => print!("{}", others),
+            for y in (0..size_y).rev() {
+                for x in 0..size_x {
+                    match output.get_2d(x, y).index {
+                        0 => print!("🗻"),
+                        1 => print!("🌲"),
+                        2 => print!("🌳"),
+                        3 => print!("🟩"),
+                        4 => print!("🟨"),
+                        5 => print!("🟦"), // 🌊
+                        others => print!("{}", others),
+                    }
                 }
+                println!();
             }
-            println!();
         }
     }
 }
