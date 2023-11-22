512398bb131ff552dc010d4c9a6ed9069c797052
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-11-22 17:09:56 +0100

    remove ConfiguationFailure error and update test case

--- src/lib.rs
+++ src/lib.rs
@@ -6,36 +6,41 @@ pub mod grid;
 pub enum ProcGenError {
     #[error("Failed to generate")]
     GenerationFailure,
-    #[error("Configuration failure")]
-    ConfigurationFailure,
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-        generator::{builder::GeneratorBuilder, node::NodeModel, rules::Rules},
+        generator::{
+            builder::GeneratorBuilder,
+            node::{NodeModel, NodeRotation},
+            rules::Rules,
+        },
         grid::Grid,
     };
 
     #[test]
     fn generate_test() {
+        tracing_subscriber::fmt::init();
         let models = vec![
             // corner
-            NodeModel::new_2d(vec![0], vec![0], vec![1], vec![1]),
+            NodeModel::new_2d(vec![0], vec![0], vec![1], vec![1]).with_all_rotations(),
             // cross
-            NodeModel::new_2d(vec![1], vec![1], vec![1], vec![1]),
+            NodeModel::new_2d(vec![1], vec![1], vec![1], vec![1]).with_no_rotations(),
             // empty
-            NodeModel::new_2d(vec![0], vec![0], vec![0], vec![0]),
+            NodeModel::new_2d(vec![0], vec![0], vec![0], vec![0]).with_no_rotations(),
             // line
-            NodeModel::new_2d(vec![0], vec![1], vec![0], vec![1]),
+            NodeModel::new_2d(vec![0], vec![1], vec![0], vec![1])
+                .with_rotation(NodeRotation::Rot90),
             // T intersection
-            NodeModel::new_2d(vec![0], vec![1], vec![1], vec![1]),
+            NodeModel::new_2d(vec![0], vec![1], vec![1], vec![1]).with_all_rotations(),
         ];
         let rules = Rules::new_cartesian_2d(models);
-        let grid = Grid::new_cartesian_2d(8, 8, false);
+        let grid = Grid::new_cartesian_2d(5, 5, false);
         let mut generator = GeneratorBuilder::new()
             .with_rules(rules)
             .with_grid(grid)
+            .with_max_retry_count(10)
             .build();
         generator.generate().unwrap();
     }
