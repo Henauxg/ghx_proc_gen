5c48450eae8ff9fe3c523cdfc373513ba3e9bbfd
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-11-24 07:03:38 +0100

    Examples: update unicode-terrain

--- examples/unicode_terrain/unicode-terrain.rs
+++ examples/unicode_terrain/unicode-terrain.rs
@@ -1,4 +1,12 @@
-use std::rc::Rc;
+use std::{
+    io::{stdin, stdout, Write},
+    thread, time,
+};
+
+use ghx_proc_gen::{
+    generator::{node::GeneratedNode, observer::QueuedStatefulObserver, ModelSelectionHeuristic},
+    grid::{direction::Cartesian2D, GridData},
+};
 
 use {
     ghx_proc_gen::generator::{
@@ -6,45 +14,58 @@ use {
     },
     ghx_proc_gen::grid::Grid,
 };
+
+const ICONES: &'static [&str] = &["🗻", "🌲", "🌳", "🟩", "🟨", "🟦"]; // ,
 fn main() {
     tracing_subscriber::fmt::init();
     let models = vec![
         NodeModel::new_2d(vec![3], vec![3], vec![3], vec![3]), // Mountain
         NodeModel::new_2d(vec![2, 3], vec![2, 3], vec![2, 3], vec![2, 3]).with_weight(0.5), // Forest1
         NodeModel::new_2d(vec![2, 3], vec![2, 3], vec![2, 3], vec![2, 3]).with_weight(0.5), // Forest2
-        NodeModel::new_2d(vec![2, 1], vec![2, 1], vec![2, 1], vec![2, 1]), // Meadows
-        NodeModel::new_2d(vec![0], vec![0], vec![0], vec![0]).with_weight(1.5), // Sea
-        NodeModel::new_2d(vec![0], vec![0, 1], vec![0, 1], vec![0, 1])
-            .with_weight(0.25)
-            .with_all_rotations(), // Beach
+        NodeModel::new_2d(vec![2, 1], vec![2, 1], vec![2, 1], vec![2, 1]), // // Meadows
+        NodeModel::new_2d(vec![0, 1], vec![0, 1], vec![0, 1], vec![0, 1]), // Beach
+        NodeModel::new_2d(vec![0], vec![0], vec![0], vec![0]),             // Sea
     ];
-    let rules = Rc::new(Rules::new_cartesian_2d(models));
-    let repeat_count = 1;
-    for _ in 0..repeat_count {
-        let size_x = 11;
-        let size_y = 8;
-        let grid = Grid::new_cartesian_2d(size_x, size_y, false);
-        let mut generator = GeneratorBuilder::new()
-            .with_shared_rules(Rc::clone(&rules))
-            .with_grid(grid)
-            .with_max_retry_count(750)
-            .with_rng(RngMode::Random)
-            .with_node_heuristic(NodeSelectionHeuristic::MinimumRemainingValue)
-            .build();
-        let output = generator.generate().unwrap();
+    let rules = Rules::new_cartesian_2d(models);
+    let grid = Grid::new_cartesian_2d(45, 20, false);
+    let size = grid.total_size();
+    let mut generator = GeneratorBuilder::new()
+        .with_rules(rules)
+        .with_grid(grid)
+        .with_max_retry_count(750)
+        .with_rng(RngMode::Random)
+        .with_node_heuristic(NodeSelectionHeuristic::MinimumRemainingValue)
+        .with_model_heuristic(ModelSelectionHeuristic::WeightedProbability)
+        .build();
+    let mut observer = QueuedStatefulObserver::new(&mut generator);
 
-        for y in (0..size_y).rev() {
-            for x in 0..size_x {
-                match output.get_2d(x, y).index {
-                    0 => print!("🗻"),
-                    1 => print!("🌲"),
-                    2 => print!("🌳"),
-                    3 => print!("🟩"),
-                    4 => print!("🟦"), // 🌊
-                    _ => print!("🟨"),
-                }
-            }
-            println!();
-        }
+    for i in 1..=size {
+        generator.select_and_propagate().unwrap();
+        observer.update();
+        println!("Grid at step {}:", i);
+        display_grid(observer.grid_data());
+        //  pause();
+
+        // thread::sleep(time::Duration::from_millis(400));
     }
+}
+
+fn display_grid(data_grid: &GridData<Cartesian2D, Option<GeneratedNode>>) {
+    for y in (0..data_grid.grid().size_y()).rev() {
+        for x in 0..data_grid.grid().size_x() {
+            match data_grid.get_2d(x, y) {
+                None => print!("❓"),
+                Some(node) => print!("{}", ICONES[node.index]),
+            }
+        }
+        println!();
+    }
+}
+
+fn pause() {
+    let mut word = String::new();
+    let mut stdout = stdout();
+    stdout.write(b"Press Enter to continue").unwrap();
+    stdout.flush().unwrap();
+    stdin().read_line(&mut word).unwrap();
 }
