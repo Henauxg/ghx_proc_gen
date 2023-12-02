0a52febae63ca29ae94daf78ff8c51566449f011
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-12-02 18:25:54 +0100

    [proc_gen] Add a QueuedObserver

--- ghx_proc_gen/src/generator/observer.rs
+++ ghx_proc_gen/src/generator/observer.rs
@@ -29,6 +29,7 @@ impl<T: DirectionSet + Clone> QueuedStatefulObserver<T> {
         &self.grid_data
     }
 
+    /// Updates the internal state of the observer by dequeuing all queued updates.
     pub fn update(&mut self) {
         while let Ok(update) = self.receiver.try_recv() {
             self.grid_data
@@ -36,15 +37,50 @@ impl<T: DirectionSet + Clone> QueuedStatefulObserver<T> {
         }
     }
 
-    /// Returns true if there was as update to process.
-    pub fn update_one_step(&mut self) -> bool {
+    /// Updates the internal state of the observer by dequeuing 1 queued update.
+    ///
+    /// Returns [`Some(GenerationUpdate)`] if there was an update to process, else returns `None`.
+    pub fn update_one_step(&mut self) -> Option<GenerationUpdate> {
         match self.receiver.try_recv() {
             Ok(update) => {
                 self.grid_data
                     .set(update.node_index, Some(update.generated_node));
-                true
+                Some(update)
             }
-            Err(_) => false,
+            Err(_) => None,
+        }
+    }
+}
+
+pub struct QueuedObserver {
+    receiver: crossbeam_channel::Receiver<GenerationUpdate>,
+}
+
+impl QueuedObserver {
+    pub fn new<T: DirectionSet + Clone>(generator: &mut Generator<T>) -> Self {
+        let receiver = generator.add_observer_queue();
+        QueuedObserver { receiver }
+    }
+
+    /// Dequeues all queued updates.
+    ///
+    /// Returns all retrieved [`GenerationUpdate`] in a `Vec`.
+    /// The `Vec` may be empty if no update was queued.
+    pub fn update(&mut self) -> Vec<GenerationUpdate> {
+        let mut updates = Vec::new();
+        while let Ok(update) = self.receiver.try_recv() {
+            updates.push(update);
+        }
+        updates
+    }
+
+    /// Dequeues 1 queued update.
+    ///
+    /// Returns [`Some(GenerationUpdate)`] if there was an update to process, else returns `None`.
+    pub fn update_one_step(&mut self) -> Option<GenerationUpdate> {
+        match self.receiver.try_recv() {
+            Ok(update) => Some(update),
+            Err(_) => None,
         }
     }
 }
