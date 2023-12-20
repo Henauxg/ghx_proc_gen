0de4b4781faa374a99fbc5e3b52375b13d021b6a
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-12-20 16:23:19 +0100

    [bevy-examples] Update the tiles example

--- bevy_examples/tiles/tiles.rs
+++ bevy_examples/tiles/tiles.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, time::Duration};
+use std::collections::HashMap;
 
 use bevy::{log::LogPlugin, prelude::*};
 
 use bevy_examples::{
-    spawn_node, step_by_step_input_update, step_by_step_timed_update, Generation, GenerationTimer,
-    GenerationViewMode,
+    anim::{ease_in_cubic, SpawningScaleAnimation},
+    plugin::{sprite_node_spawner, ProcGenExamplesPlugin},
+    Generation, GenerationViewMode,
 };
 use bevy_ghx_proc_gen::{
     grid::Grid,
@@ -22,14 +23,18 @@ use crate::rules::rules_and_assets;
 
 mod rules;
 
-/// Change this value to change the way the generation is visualized
-const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::Final;
-
+// --------------------------------------------
+/// Change this to change the map size.
 const GRID_X: u32 = 20;
 const GRID_Y: u32 = 20;
 
+/// Change this value to change the way the generation is visualized
+const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::StepByStep(15);
+// --------------------------------------------
+
 /// Size of a block in world units
 const TILE_SIZE: f32 = 32.;
+const NODE_SIZE: Vec3 = Vec3::new(TILE_SIZE, TILE_SIZE, 1.);
 const ASSETS_PATH: &str = "tiles";
 
 fn setup_scene(mut commands: Commands) {
@@ -67,64 +72,42 @@ fn setup_generator(mut commands: Commands, asset_server: Res<AssetServer>) {
         }
     }
 
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStepPaused => (),
-        GenerationViewMode::StepByStep(interval) => commands.insert_resource(GenerationTimer(
-            Timer::new(Duration::from_millis(interval), TimerMode::Repeating),
-        )),
-        GenerationViewMode::Final => {
-            let output = generator.generate().unwrap();
-            for (node_index, node) in output.nodes().iter().enumerate() {
-                spawn_node(
-                    &mut commands,
-                    &models_assets,
-                    generator.grid(),
-                    node,
-                    node_index,
-                    TILE_SIZE,
-                );
-            }
-        }
-    }
+    let grid_entity = commands
+        .spawn((
+            SpatialBundle::from_transform(Transform::from_translation(Vec3 {
+                x: -TILE_SIZE * grid.size_x() as f32 / 2.,
+                y: -TILE_SIZE * grid.size_y() as f32 / 2.,
+                z: 0.,
+            })),
+            Grid { def: grid },
+        ))
+        .id();
 
     commands.insert_resource(Generation {
         models_assets,
         gen: generator,
         observer,
-        node_size: TILE_SIZE,
-    });
+        node_scale: NODE_SIZE,
+        grid_entity,
 
-    commands.spawn((
-        SpatialBundle::from_transform(Transform::from_translation(Vec3 {
-            x: 0.,
-            y: 0.,
-            z: 0.,
-        })),
-        Grid { def: grid },
-    ));
+        assets_initial_scale: Vec3::ZERO,
+        bundle_spawner: sprite_node_spawner,
+        spawn_animation: Some(SpawningScaleAnimation::new(0.4, Vec3::ONE, ease_in_cubic)),
+    });
 }
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(
+    app.add_plugins((
         DefaultPlugins
             .set(LogPlugin {
                 filter: "info,wgpu_core=warn,wgpu_hal=warn,ghx_proc_gen=debug".into(),
                 level: bevy::log::Level::DEBUG,
             })
             .set(ImagePlugin::default_nearest()),
-    );
+        ProcGenExamplesPlugin::<Cartesian2D, Image, SpriteBundle>::new(GENERATION_VIEW_MODE),
+    ));
     app.add_systems(Startup, (setup_generator, setup_scene));
-
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStep(_) => {
-            app.add_systems(Update, step_by_step_timed_update::<Cartesian2D>);
-        }
-        GenerationViewMode::StepByStepPaused => {
-            app.add_systems(Update, step_by_step_input_update::<Cartesian2D>);
-        }
-        GenerationViewMode::Final => (),
-    };
 
     app.run();
 }
