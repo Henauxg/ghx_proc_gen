30e1be55e1770b74cbc25040f6cd8943c95115cf
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-12-20 16:17:33 +0100

    [bevy_examples:tile-layers] Use the examples plugin

--- bevy_examples/tile-layers/tile-layers.rs
+++ bevy_examples/tile-layers/tile-layers.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, time::Duration};
+use std::collections::HashMap;
 
 use bevy::{log::LogPlugin, prelude::*};
 
 use bevy_examples::{
-    spawn_node, step_by_step_input_update, step_by_step_timed_update, Generation, GenerationTimer,
-    GenerationViewMode,
+    anim::{ease_in_cubic, SpawningScaleAnimation},
+    plugin::{sprite_node_spawner, ProcGenExamplesPlugin},
+    Generation, GenerationViewMode,
 };
 use bevy_ghx_proc_gen::{
     grid::Grid,
@@ -25,16 +26,21 @@ use crate::rules::rules_and_assets;
 
 mod rules;
 
+// --------------------------------------------
+/// Change this to change the map size.
 const GRID_X: u32 = 20;
 const GRID_Y: u32 = 20;
-const GRID_Z: u32 = 4;
 
 /// Change this value to change the way the generation is visualized
-const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::Final;
+const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::StepByStep(10);
+// --------------------------------------------
 
 /// Size of a block in world units
 const TILE_SIZE: f32 = 32.;
+const NODE_SIZE: Vec3 = Vec3::new(TILE_SIZE, TILE_SIZE, 1.);
 const ASSETS_PATH: &str = "tile_layers";
+/// Number of z layers in the map, do not chnage without adapting the rules.
+const GRID_Z: u32 = 4;
 
 fn setup_scene(mut commands: Commands) {
     // Camera
@@ -52,83 +58,62 @@ fn setup_generator(mut commands: Commands, asset_server: Res<AssetServer>) {
         .unwrap();
     let grid = GridDefinition::new_cartesian_3d(GRID_X, GRID_Y, GRID_Z, false);
     // Create generator
-    let mut generator = GeneratorBuilder::new()
+    let mut gen = GeneratorBuilder::new()
         .with_rules(rules)
         .with_grid(grid.clone())
         .with_rng(RngMode::RandomSeed)
         .with_node_heuristic(NodeSelectionHeuristic::MinimumRemainingValue)
         .with_model_heuristic(ModelSelectionHeuristic::WeightedProbability)
         .build();
-    let observer = QueuedObserver::new(&mut generator);
-    info!("Seed: {}", generator.get_seed());
+    let observer = QueuedObserver::new(&mut gen);
+    info!("Seed: {}", gen.get_seed());
 
     // Load assets
     let mut models_assets = HashMap::new();
-    for (index, path) in models_asset_paths.iter().enumerate() {
+    for (model_index, path) in models_asset_paths.iter().enumerate() {
         if let Some(path) = path {
             models_assets.insert(
-                index,
+                model_index,
                 asset_server.load(format!("{ASSETS_PATH}/{path}.png")),
             );
         }
     }
 
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStepPaused => (),
-        GenerationViewMode::StepByStep(interval) => commands.insert_resource(GenerationTimer(
-            Timer::new(Duration::from_millis(interval), TimerMode::Repeating),
-        )),
-        GenerationViewMode::Final => {
-            let output = generator.generate().unwrap();
-            for (node_index, node) in output.nodes().iter().enumerate() {
-                spawn_node(
-                    &mut commands,
-                    &models_assets,
-                    generator.grid(),
-                    node,
-                    node_index,
-                    TILE_SIZE,
-                );
-            }
-        }
-    }
+    let grid_entity = commands
+        .spawn((
+            SpatialBundle::from_transform(Transform::from_translation(Vec3 {
+                x: -TILE_SIZE * grid.size_x() as f32 / 2.,
+                y: -TILE_SIZE * grid.size_y() as f32 / 2.,
+                z: 0.,
+            })),
+            Grid { def: grid },
+        ))
+        .id();
 
     commands.insert_resource(Generation {
         models_assets,
-        gen: generator,
+        gen,
         observer,
-        node_size: TILE_SIZE,
-    });
+        node_scale: NODE_SIZE,
+        grid_entity,
 
-    commands.spawn((
-        SpatialBundle::from_transform(Transform::from_translation(Vec3 {
-            x: 0.,
-            y: 0.,
-            z: 0.,
-        })),
-        Grid { def: grid },
-    ));
+        assets_initial_scale: Vec3::ZERO,
+        bundle_spawner: sprite_node_spawner,
+        spawn_animation: Some(SpawningScaleAnimation::new(0.4, Vec3::ONE, ease_in_cubic)),
+    });
 }
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins((DefaultPlugins
-        .set(LogPlugin {
-            filter: "info,wgpu_core=warn,wgpu_hal=warn,ghx_proc_gen=debug".into(),
-            level: bevy::log::Level::DEBUG,
-        })
-        .set(ImagePlugin::default_nearest()),));
+    app.add_plugins((
+        DefaultPlugins
+            .set(LogPlugin {
+                filter: "info,wgpu_core=warn,wgpu_hal=warn,ghx_proc_gen=debug".into(),
+                level: bevy::log::Level::DEBUG,
+            })
+            .set(ImagePlugin::default_nearest()),
+        ProcGenExamplesPlugin::<Cartesian3D, Image, SpriteBundle>::new(GENERATION_VIEW_MODE),
+    ));
     app.add_systems(Startup, (setup_generator, setup_scene));
-
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStep(_) => {
-            app.add_systems(Update, step_by_step_timed_update::<Cartesian3D>);
-        }
-        GenerationViewMode::StepByStepPaused => {
-            app.add_systems(Update, step_by_step_input_update::<Cartesian3D>);
-        }
-        GenerationViewMode::Final => (),
-    };
-
     app.run();
 }
