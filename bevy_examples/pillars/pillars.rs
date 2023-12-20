79f6b52be55ff745d24345480174ce5b752d4551
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-12-20 16:29:44 +0100

    [bevy-examples:pillars] Update to use examples plugin

--- bevy_examples/pillars/pillars.rs
+++ bevy_examples/pillars/pillars.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Duration};
+use std::collections::HashMap;
 
 use bevy::{
     log::LogPlugin,
@@ -6,17 +6,19 @@ use bevy::{
     prelude::*,
 };
 
-use bevy_examples::{GenerationTimer, GenerationViewMode};
+use bevy_examples::{
+    anim::{ease_in_cubic, SpawningScaleAnimation},
+    plugin::{scene_node_spawner, ProcGenExamplesPlugin},
+    utils::toggle_debug_grid_visibility,
+    Generation, GenerationViewMode,
+};
 use bevy_ghx_proc_gen::{
-    grid::{spawn_debug_grids, DebugGridView, DebugGridViewConfig, Grid, SharableDirectionSet},
+    grid::{spawn_debug_grids, DebugGridViewConfig, Grid},
     lines::LineMaterial,
     proc_gen::{
         generator::{
-            builder::GeneratorBuilder,
-            node::GeneratedNode,
-            observer::{GenerationUpdate, QueuedObserver},
-            rules::RulesBuilder,
-            GenerationStatus, Generator, ModelSelectionHeuristic, NodeSelectionHeuristic, RngMode,
+            builder::GeneratorBuilder, observer::QueuedObserver, rules::RulesBuilder,
+            ModelSelectionHeuristic, NodeSelectionHeuristic, RngMode,
         },
         grid::{direction::Cartesian3D, GridDefinition},
     },
@@ -27,16 +29,18 @@ use crate::rules::rules_and_assets;
 
 mod rules;
 
+// --------------------------------------------
 /// Change this value to change the way the generation is visualized
-const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::Final;
+const GENERATION_VIEW_MODE: GenerationViewMode = GenerationViewMode::StepByStep(15);
 
+/// Change this to change the map size.
 const GRID_HEIGHT: u32 = 7;
 const GRID_X: u32 = 80;
 const GRID_Z: u32 = 80;
+// --------------------------------------------
 
 /// Size of a block in world units
 const NODE_SIZE: f32 = 1.;
-const HALF_NODE_SIZE: f32 = NODE_SIZE / 2.;
 const NODE_SCALE: Vec3 = Vec3::new(NODE_SIZE, NODE_SIZE, NODE_SIZE);
 
 const ASSETS_SCALE_FACTOR: f32 = NODE_SIZE / 4.; // Models are 4 units wide
@@ -46,15 +50,6 @@ const ASSETS_SCALE: Vec3 = Vec3::new(
     ASSETS_SCALE_FACTOR,
 );
 
-#[derive(Resource)]
-pub struct Generation<T: SharableDirectionSet> {
-    pub models_assets: HashMap<usize, Handle<Scene>>,
-    pub gen: Generator<T>,
-    pub observer: QueuedObserver,
-    /// Size of a node in world units
-    pub node_size: f32,
-}
-
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -62,7 +57,7 @@ fn setup_scene(
 ) {
     // Camera
     let camera_position = Vec3::new(-2.5, 1.5, 9.0);
-    let radius = camera_position.length();
+    let _radius = camera_position.length();
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_translation(camera_position).looking_at(Vec3::ZERO, Vec3::Y),
@@ -174,168 +169,35 @@ fn setup_generator(mut commands: Commands, asset_server: Res<AssetServer>) {
         }
     }
 
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStepPaused => (),
-        GenerationViewMode::StepByStep(interval) => commands.insert_resource(GenerationTimer(
-            Timer::new(Duration::from_millis(interval), TimerMode::Repeating),
-        )),
-        GenerationViewMode::Final => {
-            let output = generator.generate().unwrap();
-            for (node_index, node) in output.nodes().iter().enumerate() {
-                spawn_node(
-                    &mut commands,
-                    &models_assets,
-                    generator.grid(),
-                    node,
-                    node_index,
-                );
-            }
-        }
-    }
+    let grid_entity = commands
+        .spawn((
+            SpatialBundle::from_transform(Transform::from_translation(Vec3 {
+                x: -(grid.size_x() as f32) / 2.,
+                y: 0.,
+                z: -(grid.size_z() as f32) / 2.,
+            })),
+            Grid { def: grid },
+            DebugGridViewConfig {
+                node_size: NODE_SCALE,
+                color: Color::GRAY.with_a(0.),
+            },
+        ))
+        .id();
 
     commands.insert_resource(Generation {
         models_assets,
         gen: generator,
         observer,
-        node_size: NODE_SIZE,
+        node_scale: NODE_SCALE,
+        grid_entity,
+        assets_initial_scale: Vec3::ZERO,
+        spawn_animation: Some(SpawningScaleAnimation::new(
+            0.8,
+            ASSETS_SCALE,
+            ease_in_cubic,
+        )),
+        bundle_spawner: scene_node_spawner,
     });
-
-    commands.spawn((
-        SpatialBundle::from_transform(Transform::from_translation(Vec3 {
-            x: -(grid.size_x() as f32) / 2.,
-            y: 0.,
-            z: grid.size_z() as f32 / 2.,
-        })),
-        Grid { def: grid },
-        DebugGridViewConfig {
-            node_size: NODE_SCALE,
-            color: Color::GRAY.with_a(0.),
-        },
-    ));
-}
-
-#[derive(Component)]
-struct SpawnedNode;
-
-fn spawn_node<T: SharableDirectionSet>(
-    commands: &mut Commands,
-    models_assets: &HashMap<usize, Handle<Scene>>,
-    grid: &GridDefinition<T>,
-    node: &GeneratedNode,
-    node_index: usize,
-) {
-    if let Some(asset) = models_assets.get(&node.model_index) {
-        let x_offset = grid.size_x() as f32 / 2.;
-        let z_offset = grid.size_z() as f32 / 2.;
-        let pos = grid.get_position(node_index);
-        commands.spawn((
-            SceneBundle {
-                scene: asset.clone(),
-                transform: Transform::from_xyz(
-                    (pos.x as f32) - x_offset + HALF_NODE_SIZE,
-                    pos.y as f32,
-                    (pos.z as f32) - z_offset + HALF_NODE_SIZE,
-                )
-                .with_scale(ASSETS_SCALE)
-                .with_rotation(Quat::from_rotation_y(f32::to_radians(
-                    node.rotation.value() as f32,
-                ))),
-                ..default()
-            },
-            SpawnedNode,
-        ));
-    }
-}
-
-fn select_and_propagate<T: SharableDirectionSet>(
-    commands: &mut Commands,
-    generation: &mut ResMut<Generation<T>>,
-    nodes: Query<Entity, With<SpawnedNode>>,
-) {
-    match generation.gen.select_and_propagate() {
-        Ok(status) => match status {
-            GenerationStatus::Ongoing => (),
-            GenerationStatus::Done => info!("Generation done"),
-        },
-        Err(_) => {
-            info!("Generation Failed")
-        }
-    }
-    // Process the observer queue even if generation failed
-    let updates = generation.observer.dequeue_all();
-    // Buffer the nodes to spawn in case a generation failure invalidate some.
-    let mut nodes_to_spawn = vec![];
-    let mut despawn_nodes = false;
-    for update in updates {
-        match update {
-            GenerationUpdate::Generated {
-                node_index,
-                generated_node,
-            } => {
-                nodes_to_spawn.push((node_index, generated_node));
-            }
-            GenerationUpdate::Reinitialized | GenerationUpdate::Failed => {
-                nodes_to_spawn.clear();
-                despawn_nodes = true;
-            }
-        }
-    }
-    if despawn_nodes {
-        for entity in nodes.iter() {
-            commands.entity(entity).despawn_recursive();
-        }
-    }
-
-    for (node_index, generated_node) in nodes_to_spawn {
-        info!("Spawning {:?} at node index {}", generated_node, node_index);
-        spawn_node(
-            commands,
-            &generation.models_assets,
-            generation.gen.grid(),
-            &generated_node,
-            node_index,
-        );
-    }
-}
-
-fn step_by_step_input_update<T: SharableDirectionSet>(
-    mut commands: Commands,
-    keys: Res<Input<KeyCode>>,
-    buttons: Res<Input<MouseButton>>,
-    mut generation: ResMut<Generation<T>>,
-    nodes: Query<Entity, With<SpawnedNode>>,
-) {
-    if keys.pressed(KeyCode::Space) || buttons.just_pressed(MouseButton::Left) {
-        select_and_propagate(&mut commands, &mut generation, nodes);
-    }
-}
-
-fn step_by_step_timed_update<T: SharableDirectionSet>(
-    mut commands: Commands,
-    mut generation: ResMut<Generation<T>>,
-    mut timer: ResMut<GenerationTimer>,
-    time: Res<Time>,
-    nodes: Query<Entity, With<SpawnedNode>>,
-) {
-    timer.0.tick(time.delta());
-    if timer.0.finished() {
-        select_and_propagate(&mut commands, &mut generation, nodes);
-    }
-}
-
-fn toggle_debug_grid_visibility(
-    keys: Res<Input<KeyCode>>,
-    mut debug_grids: Query<&mut Visibility, With<DebugGridView>>,
-) {
-    if keys.just_pressed(KeyCode::F1) {
-        for mut view_visibility in debug_grids.iter_mut() {
-            *view_visibility = match *view_visibility {
-                Visibility::Inherited => Visibility::Hidden,
-                Visibility::Hidden => Visibility::Visible,
-                Visibility::Visible => Visibility::Hidden,
-            }
-        }
-    }
 }
 
 fn main() {
@@ -347,21 +209,12 @@ fn main() {
             level: bevy::log::Level::DEBUG,
         }),
         MaterialPlugin::<LineMaterial>::default(),
+        ProcGenExamplesPlugin::<Cartesian3D, Scene, SceneBundle>::new(GENERATION_VIEW_MODE),
     ));
     app.add_systems(Startup, (setup_generator, setup_scene))
         .add_systems(Update, pan_orbit_camera)
         .add_systems(Update, spawn_debug_grids::<Cartesian3D>)
         .add_systems(Update, toggle_debug_grid_visibility);
-
-    match GENERATION_VIEW_MODE {
-        GenerationViewMode::StepByStep(_) => {
-            app.add_systems(Update, step_by_step_timed_update::<Cartesian3D>);
-        }
-        GenerationViewMode::StepByStepPaused => {
-            app.add_systems(Update, step_by_step_input_update::<Cartesian3D>);
-        }
-        GenerationViewMode::Final => (),
-    };
 
     app.run();
 }
